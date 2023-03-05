96100bab4062d6b7730a808b4f380786f7befaf1
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-03-05 11:39:27 +0530

    added functionalities using own logic

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+// Select required elements
+
+var modalBtn = document.querySelector('.modal-btn');
+var modal = document.querySelector('.modal-overlay');
+var closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', modifyModal);
+closeBtn.addEventListener('click', modifyModal);
+
+function modifyModal(){
+    modal.classList.toggle('open-modal');
+}
+
